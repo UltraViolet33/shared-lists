8d512cf43fe916feb764293371cb49bf6e423bfd
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2025-02-17 09:41:58 +0100

    add docker compose

--- config.py
+++ config.py
@@ -6,12 +6,7 @@ load_dotenv(os.path.join(project_folder, '.env'))
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-DB_NAME = os.getenv('DB_NAME')
-DB_USERNAME = os.getenv('DB_USERNAME')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 class Config(object):
     FLASK_ENV = 'development'
@@ -19,7 +14,7 @@ class Config(object):
     TESTING = False
     SECRET_KEY = os.getenv('SECRET_KEY', default='BAD_SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
