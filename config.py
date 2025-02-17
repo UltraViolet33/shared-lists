f5430f76dd78f099df0547a3151301584c29a8ef
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2025-02-17 10:28:47 +0100

    edit config

--- config.py
+++ config.py
@@ -6,15 +6,19 @@ load_dotenv(os.path.join(project_folder, ".env"))
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_NAME = os.getenv('DB_NAME')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 
 class Config(object):
-    FLASK_ENV = "development"
+    FLASK_ENV = 'development'
     DEBUG = False
     TESTING = False
-    SECRET_KEY = os.getenv("SECRET_KEY", default="BAD_SECRET_KEY")
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = f'mysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
